d8c3216307349f78d2e907d04f2221fb09e2be61
pothikit <tytanhso71.@gmail.com>
2022-04-19 18:58:12 +0600

    home section updated

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+// let nextBtn = document.getElementsByClassName('arrow-right');
+// let prevBtn = document.getElementsByClassName('arrow-left');
+
+// nextBtn.addEventListener("click",function(){
+//     nextSlide();
+// })
+
+
+
+// let slideNumber = 0;
+
+// const nextSlide = () => {
+//     const sliderMain = document.getElementById("slider");
+//     const sliderItem = document.getElementsByClassName("slider-item");
+//     const currentSlide = document.getElementsByClassName("current");
+
+//     // logic
+//     currentSlide[0].classList.remove("current");
+// }
+
+// console.log(currentSlide);
